b8bcfd665ab580f8c76e983895f8151c91096477
napescui <rokagaming78@gmail.com>
2025-07-10 14:38:36 +0700

    sync full local to remote

--- AutoForce.sh
+++ AutoForce.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
 git branch -M main
-
 git remote set-url origin https://github.com/napescui/stocksage.git
 
+# Tambah semua perubahan, termasuk yang dihapus
 git add -A
-git commit -m "sync full local to remote"
--A = tambah semua, termasuk file yang dihapus.
 
+# Commit perubahan
+git commit -m "sync full local to remote"
+
+# Force push ke remote
 git push -f origin main
